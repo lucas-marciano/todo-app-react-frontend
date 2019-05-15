a8c1f9d8d67e98676f3cd96ed2ab78746332d23f
Lucas Marciano <lucasmarciano@inatel.br>
2019-05-15 18:14:45 -0300

    (feat): implemented: list, routes, insert and delet function, with axios comunicated with server.

--- src/todo/list.jsx
+++ src/todo/list.jsx
@@ -1,7 +1,32 @@
-import React from "react"
+import React from "react";
+import IButton from "../template/buttons/iconButton";
 
-export default props => (
-    <div>
-        <h3>List</h3>
-    </div>
-)
+export default props => {
+  const renderRows = () => {
+    const list = props.list || [];
+    return list.map(todo => (
+      <tr key={todo._id}>
+        <td>{todo.description}</td>
+        <td>
+          <IButton
+            style="danger"
+            icon="trash-o"
+            onClick={() => props.handleRemove(todo._id)}
+          />
+        </td>
+      </tr>
+    ));
+  };
+
+  return (
+    <table className="table">
+      <thead>
+        <tr>
+          <th>Description</th>
+          <th>Actions</th>
+        </tr>
+      </thead>
+      <tbody>{renderRows()}</tbody>
+    </table>
+  );
+};
